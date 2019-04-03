e78f1685b9cd3dd4eb90b18bcfd383df5e986d4d
Thompson Galetovic <200conceptz@gmail.com>
2019-04-03 12:30:01 -0400

    updateItem test commented out

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,16 @@ $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 
-  api.getItems()
-    .then(res => res.json())
-    .then((items) => {
-      const item = items[0];
-      return api.updateItem(item.id, {name: 'foobar'});
-    })
-    .then(res => res.json())
-    .then(() => console.log('updated!'));
- 
+  // api.getItems()
+  //   .then(res => res.json())
+  //   .then((items) => {
+  //     const item = items[0];
+  //     return api.updateItem(item.id, {name: 'foobar'});
+  //   })
+  //   .then(res => res.json())
+  //   .then(() => console.log('updated!'));
+ // #test for api.updateItem
+
     api.getItems()
     .then(res => res.json())
     .then((items) => {
